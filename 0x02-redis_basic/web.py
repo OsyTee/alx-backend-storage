3182053b0cdaf6ccdb17aad6b0c59f6cb73010b4
OsyTee <OsyTee@github.com>
2023-07-20 10:24:22 +0000

    Advanced Task

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,68 @@
 #!/usr/bin/env python3
+
 """
-web cache and tracker
+This module provides functions for retrieving
+web pages and caching the results.
 """
+
 import requests
-import redis
+import time
 from functools import wraps
-
-store = redis.Redis()
-
-
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
+from typing import Dict
 
 
-@count_url_access
+def cached(expiration_time: int):
+    """
+    Decorator to cache the result of a function with an expiration time.
+
+    Args:
+        expiration_time: The time in seconds until the cache expires.
+
+    Returns:
+        The decorated function.
+    """
+    def decorator(func):
+        cache = {}
+
+        @wraps(func)
+        def wrapper(url):
+            if url in cache and \
+                    time.time() - cache[url]['timestamp'] < expiration_time:
+                cache[url]['count'] += 1
+                return cache[url]['content']
+
+            content = func(url)
+            cache[url] = {
+                'content': content,
+                'timestamp': time.time(),
+                'count': 1
+            }
+            return content
+
+        return wrapper
+
+    return decorator
+
+
+@cached(expiration_time=10)
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """
+    Retrieve the HTML content of a web page.
+
+    Args:
+        url: The URL of the web page.
+
+    Returns:
+        The HTML content of the web page as a string.
+    """
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == '__main__':
+    # Example usage
+    url = ('http://slowwly.robertomurray.co.uk/delay/1000/url/'
+           'http://www.google.co.uk')
+    for _ in range(5):
+        print(get_page(url))
+        time.sleep(2)
